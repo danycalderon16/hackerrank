c8ca0e3bb308c09d3f91a773ada62b6660abe2ea
Daniel Calderon <danycalvir@gmail.com>
2024-02-26 22:24:01 -0700

    simple sum in python

---
+++ simpleArraySum.ts
@@ -0,0 +1,5 @@
+function simpleArraySum(ar: number[]): number {
+  return ar.reduce((value, acc)=> value + acc, 0)
+}
+
+console.log(simpleArraySum([1,2,3,4,10,11]));
---
+++ simple_array_sum.py
@@ -0,0 +1,12 @@
+def simple_array_sum(ar):
+    # accun = 0
+    # for i in range(len(ar)):
+    #     accun = ar[i] + accun
+
+    # return accun
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    result = simple_array_sum([1, 2, 3, 4, 10, 11])
+    print(result)
