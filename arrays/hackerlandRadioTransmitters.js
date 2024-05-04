8f706b684abd33cad0e33acbff4ccf176d3621da
Daniel Calderon <danycalvir@gmail.com>
2024-05-04 14:11:44 -0700

    hackerlandRadioTransmitters

--- arrays/hackerlandRadioTransmitters.js
+++ arrays/hackerlandRadioTransmitters.js
@@ -1,27 +1,19 @@
 function hackerlandRadioTransmitters(x = [], k = 0) {
-  // Write your code here
-  // k = 5
-  // i  0 1 2 3 4 5
-  // sa 1 2 3 4 5 10
-  // c = 10
-  // fh = 0
-  // acc = 1
-  //         c    sa[i]  sa[fh]
-  // i = 1 | 10 - (2   -   1   ) = 9  - false + falso = falso
-  // i = 2 | 10 - (3   -   1   ) = 8  - false + falso = falso
-  // i = 3 | 10 - (4   -   1   ) = 7  - false + falso = falso
-  // i = 4 | 10 - (5   -   1   ) = 6  - false + falso = falso
-  // i = 5 | 10 - (10  -  1   )  = 1  - false + true = true
-
-  const cover = k * 2;
   const sa = x.sort((a, b) => a - b);
-  let acc = 1;
-  let fh = 0; // first house
-  for (let i = 1; i < sa.length; i++) {
-    if (cover - (sa[i] - sa[fh]) <= 0) {
-      acc += 1;
-      fh = i + 1;
+  const n = sa.length;
+  let acc = 0;
+  let i = 0;
+
+  while (i < n) {
+    let cover = sa[i];
+    while (i < n && sa[i] <= cover + k) {
+      i++;
     }
+    cover = sa[i - 1];
+    while (i < n && sa[i] <= cover + k) {
+      i++;
+    }
+    acc++;
   }
 
   return acc;
