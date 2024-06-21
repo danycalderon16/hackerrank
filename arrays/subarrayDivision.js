ff43bca688c65d6e3a5391a3efc5abeb3247165b
Daniel Calderon <danycalvir@gmail.com>
2024-06-20 18:35:15 -0700

    countWaysToDivideChocolate js

--- arrays/subarrayDivision.js
+++ arrays/subarrayDivision.js
@@ -8,4 +8,17 @@ The sum of the integers on the squares is equal to his birth day.
 Determine how many ways she can divide the chocolate.
  * **/
 
-function countWaysToDivideChocolate(s, d, m) {}
+function countWaysToDivideChocolate(s = [], d = 0, m = 0) {
+  let rigth = 0;
+
+  let w = 0;
+  for (let i = 0; i < s.length; i++) {
+    rigth = i + m;
+    const acc = s.slice(i, rigth).reduce((a, b) => a + b, 0);
+    if (acc === d) w++;
+  }
+
+  return w;
+}
+
+console.log(countWaysToDivideChocolate([1, 2, 1, 3, 2], 3, 2));
